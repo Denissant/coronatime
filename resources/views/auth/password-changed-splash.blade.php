8f42bf4428c0780ad5b8cbae80848a454d595872
Denis Santuryan <denissanturyan@gmail.com>
2022-11-21 16:33:35 +0400

    style: success splash-screen pages

---
+++ resources/views/auth/password-changed-splash.blade.php
@@ -0,0 +1,14 @@
+<x-base>
+    <x-form.wrapper-center>
+
+            <div class="mt-[25vh] sm:mt-[16vh]">
+                <div class="mx-auto w-min mb-4">
+                    <x-assets.success />
+                </div>
+                <div class="font-normal w-2/3 mx-auto sm:w-full">Your password has been updated successfully</div>
+            </div>
+            <a href="{{ route('login') }}" class="mx-auto mt-auto sm:mt-24 w-full sm:h-auto sm:max-w-sm">
+                <x-form.button>Sign In</x-form.button>
+            </a>
+    </x-form.wrapper-center>
+</x-base>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::view('/login', 'auth.login')->name('login');
 Route::view('/register', 'auth.register')->name('register');
 Route::view('/forgot', 'auth.forgot')->name('forgot');
 Route::view('/reset', 'auth.change-password')->name('reset');
+Route::view('/password-splash', 'auth.password-changed-splash')->name('password-splash');
+Route::view('/email-splash', 'auth.email-confirmed-splash')->name('email-splash');
