2a188bd7d3c46d842265a56fb4592cbd7c1416e7
Denis Santuryan <denissanturyan@gmail.com>
2022-11-22 12:26:03 +0400

    refactor: extract verify route to controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Requests\EmailVerificationWithoutLoginRequest;
+use App\Http\Controllers\VerifyEmailController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,8 +29,4 @@ Route::view('/password-splash', 'auth.password-changed-splash')->name('password-
 Route::view('/email-splash', 'auth.email-confirmed-splash')->name('email-splash');
 Route::view('/confirmation-splash', 'auth.confirmation-sent-splash')->name('confirmation-splash');
 
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationWithoutLoginRequest $request) {
-	$request->fulfill();
-
-	return redirect()->route('email-splash');
-})->middleware(['signed'])->name('verification.verify');
+Route::get('/email/verify/{id}/{hash}', [VerifyEmailController::class, 'index'])->middleware(['signed'])->name('verification.verify');
