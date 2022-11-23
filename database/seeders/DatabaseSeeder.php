d3792454a6c4512c83ab5e0e79b6ce7e4acbe84b
Denis Santuryan <denissanturyan@gmail.com>
2022-11-24 03:40:49 +0400

    feat: add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Country;
+use DB;
+use GuzzleHttp\Client;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,37 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		// \App\Models\User::factory(10)->create();
+		$fetchedCountries = $this->fetchCountries();
 
-		// \App\Models\User::factory()->create([
-		//     'name' => 'Test User',
-		//     'email' => 'test@example.com',
-		// ]);
+		$global_country = Country::create([
+			'name'    => ['en' => 'Worldwide', 'ka' => 'მსოფლიოში'],
+			'code'    => 'GLOBAL',
+		]);
+		$global_country->save();
+
+		echo "Adding Country rows... \n";
+		DB::beginTransaction();
+		foreach ($fetchedCountries as $countryData)
+		{
+			$country = Country::create([
+				'name' => $countryData['name'],
+				'code' => $countryData['code'],
+			]);
+			$country->save();
+		}
+		DB::commit();
+		echo "Finished adding Country rows! \n";
+
+		echo "Adding Statistics to each Country... \n";
+		Country::updateAllStatistics(false);
+		echo "Finished adding Statistics! \n";
+	}
+
+	protected function fetchCountries()
+	{
+		$client = new Client();
+		$response = $client->request('GET', 'https://devtest.ge/countries');
+		$result = $response->getBody();
+		return json_decode($result, true);
 	}
 }
