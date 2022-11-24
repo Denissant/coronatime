55d402474f594d182a705f4842d3a9e3e8588db4
Denis Santuryan <denissanturyan@gmail.com>
2022-11-24 17:26:53 +0400

    fix: update statistics command logic

--- app/Console/Commands/UpdateStatisticsCommand.php
+++ app/Console/Commands/UpdateStatisticsCommand.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Models\Country;
+use App\Models\Statistics;
+use DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Console\Command;
 
 class UpdateStatisticsCommand extends Command
@@ -13,9 +16,42 @@ class UpdateStatisticsCommand extends Command
 
 	public function handle()
 	{
-		$this->info(date('Y-m-d H:i:s') . " Updating Country Statistics... \n");
-		Country::updateAllStatistics(true);
-		$this->info(date('Y-m-d H:i:s') . " Finished updating Country Statistics! \n");
+		DB::beginTransaction();
+		Country::getQuery()->delete();
+		foreach ($this->fetchCountries() as $countryData)
+		{
+			$country = Country::create([
+				'name' => $countryData['name'],
+				'code' => $countryData['code'],
+			]);
+			$country->save();
+
+			$this->addCountryStatistics($country->id, $country->code);
+		}
+		DB::commit();
+
+		$this->info(date('Y-m-d H:i:s') . " Country statistics have been updated successfully! \n");
 		return Command::SUCCESS;
+	}
+
+	protected function fetchCountries()
+	{
+		$response = Http::get('https://devtest.ge/countries')->body();
+		return json_decode($response, true);
+	}
+
+	protected function addCountryStatistics($countryId, $countryCode)
+	{
+		$statisticsData = $this->fetchCountryStatistics($countryCode);
+		Statistics::create(array_merge($statisticsData, ['country_id' => $countryId]))->save();
+	}
+
+	protected function fetchCountryStatistics($countryCode)
+	{
+		$response = Http::post('https://devtest.ge/get-country-statistics', [
+			'code' => $countryCode,
+		])->body();
+		$result = json_decode($response, true);
+		return array_diff_key($result, array_flip(['id', 'country', 'code', 'critical', 'created_at', 'updated_at']));
 	}
 }
