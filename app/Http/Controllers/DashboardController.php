70aab44df04ec2a75b7825637242a93711ccf50d
Denis Santuryan <denissanturyan@gmail.com>
2022-11-25 02:07:51 +0400

    feat: display worldwide stats on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Statistics;
+
 class DashboardController extends Controller
 {
 	public function home()
 	{
-		return view('dashboard.home');
+		$worldwideStats = $this->getWorldwideStats(Statistics::all());
+
+		return view('dashboard.home', ['worldwideStats' => $worldwideStats]);
 	}
 
 	public function countries()
 	{
+	}
+
+	protected function getWorldwideStats($allStats)
+	{
+		$worldwideStats = [
+			'confirmed' => 0,
+			'recovered' => 0,
+			'deaths'    => 0,
+		];
+
+		foreach ($allStats as $statistics)
+		{
+			$worldwideStats['confirmed'] += $statistics['confirmed'];
+			$worldwideStats['recovered'] += $statistics['recovered'];
+			$worldwideStats['deaths'] += $statistics['deaths'];
+		}
+
+		return $worldwideStats;
 	}
 }
--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,10 +1,10 @@
 <x-dashboard.wrapper>
     <div class="flex flex-wrap justify-between sm:flex-nowrap sm:space-x-6">
-        <x-dashboard.stat-figure index="1" caption="New cases" number="123123"
+        <x-dashboard.stat-figure index="1" caption="New cases" number="{{ $worldwideStats['confirmed'] }}"
                                  background_color_class="bg-[#EEEEFE]" text_color_class="text-brand-blue" graph_color="blue"/>
-        <x-dashboard.stat-figure index="2" caption="Recovered" number="123123"
+        <x-dashboard.stat-figure index="2" caption="Recovered" number="{{ $worldwideStats['recovered'] }}"
                                  background_color_class="bg-[#EAF8F1]" text_color_class="text-brand-lightgreen" graph_color="green"/>
-        <x-dashboard.stat-figure index="3" caption="Deaths" number="123123"
+        <x-dashboard.stat-figure index="3" caption="Deaths" number="{{ $worldwideStats['deaths'] }}"
                                  background_color_class="bg-[#FCFAEC]" text_color_class="text-brand-yellow" graph_color="yellow"/>
     </div>
     <x-dashboard.newsletter />
