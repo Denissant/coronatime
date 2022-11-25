632aba8a0f925daa4c5898ca52f63e21a798884f
Denis Santuryan <denissanturyan@gmail.com>
2022-11-25 12:15:50 +0400

    refactor: move controller function to model

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,30 +8,12 @@ class DashboardController extends Controller
 {
 	public function home()
 	{
-		$worldwideStats = $this->getWorldwideStats(Statistics::all());
+		$worldwideStats = Statistics::getWorldwideStats(Statistics::all());
 
 		return view('dashboard.home', ['worldwideStats' => $worldwideStats]);
 	}
 
 	public function countries()
 	{
-	}
-
-	protected function getWorldwideStats($allStats)
-	{
-		$worldwideStats = [
-			'confirmed' => 0,
-			'recovered' => 0,
-			'deaths'    => 0,
-		];
-
-		foreach ($allStats as $statistics)
-		{
-			$worldwideStats['confirmed'] += $statistics['confirmed'];
-			$worldwideStats['recovered'] += $statistics['recovered'];
-			$worldwideStats['deaths'] += $statistics['deaths'];
-		}
-
-		return $worldwideStats;
 	}
 }
--- app/Models/Statistics.php
+++ app/Models/Statistics.php
@@ -15,4 +15,22 @@ class Statistics extends Model
 	{
 		return $this->belongsTo(Country::class);
 	}
+
+	public static function getWorldwideStats($allStats)
+	{
+		$worldwideStats = [
+			'confirmed' => 0,
+			'recovered' => 0,
+			'deaths'    => 0,
+		];
+
+		foreach ($allStats as $statistics)
+		{
+			$worldwideStats['confirmed'] += $statistics['confirmed'];
+			$worldwideStats['recovered'] += $statistics['recovered'];
+			$worldwideStats['deaths'] += $statistics['deaths'];
+		}
+
+		return $worldwideStats;
+	}
 }
