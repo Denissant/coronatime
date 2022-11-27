8eb878b38ea0415e4528a0c04012746ef74f8868
Denis Santuryan <denissanturyan@gmail.com>
2022-11-27 14:55:52 +0400

    chore: add translation strings

---
+++ lang/ka/validation.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+	'confirmed'            => 'პაროლები არ ემთხვევა ერთმანეთს.',
+	'current_password'     => 'პაროლი არასწორია.',
+	'email'                => ':attribute არაა ვალიდური.',
+	'required'             => ':attribute-ის შეყვანა აუცილებელია.',
+	'unique'               => 'ეს :attribute დაკავებულია.',
+
+	'max' => [
+		'string'           => ':attribute უნდა შეიცავდეს მაქსიუმუმ :max სიმბოლოს.',
+	],
+	'min' => [
+		'string'           => ':attribute უნდა შეიცავდეს მინიმუმ :min სიმბოლოს.',
+	],
+
+	'attributes' => [
+		'email'            => 'ელ.ფოსტა',
+		'username'         => 'მომხმარებლის სახელი',
+		'password'         => 'პაროლი',
+		'confirm_password' => 'პაროლი',
+	],
+];
